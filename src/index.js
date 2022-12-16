15d9ede4d083adcd47e5562f69646efbf3643956
“prefolo” <webintosh@yahoo.it>
2022-12-16 14:01:13 +0100

    Do dark theme switcher

--- src/index.js
+++ src/index.js
@@ -6,3 +6,19 @@ import './style.css';
 DOMController.suscribe();
 
 Game();
+
+// Dark theme switcher
+
+const toggleSwitch = document.querySelector(
+	'.theme-switch input[type="checkbox"]'
+);
+
+function switchTheme(e) {
+	if (e.target.checked) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+	} else {
+		document.documentElement.setAttribute('data-theme', 'light');
+	}
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
