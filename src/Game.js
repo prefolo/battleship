20e8c0dee265a650e0e2262513b4aa5a211c776b
“prefolo” <webintosh@yahoo.it>
2022-12-17 07:08:21 +0100

    Finish game logic

--- src/Game.js
+++ src/Game.js
@@ -5,55 +5,42 @@ import Placeboard from './Placement/Placeboard';
 import Dock from './Placement/Dock';
 import { randomPlaceShips } from './Placement/PlacementUtils';
 
-/* const makeCpuGameboardClickable = (player, computer) => {
-	document.querySelectorAll('.cpuCell').forEach((cell) => {
-		cell.addEventListener('click', function () {
-			const coor = this.dataset.coor.split(',');
+let isGameOver = true;
+let playerGb;
+let computerGb;
 
-			const thisCellIsYetHit = player.attack(coor[0], coor[1]);
-			if (thisCellIsYetHit) return;
-
-			if (computer.gameboard.allShipsHaveBeenSunk()) {
-				PubSub.publish('Player won', {});
-				return;
-			}
-
-			computer.attack();
-
-			if (player.gameboard.allShipsHaveBeenSunk()) {
-				PubSub.publish('Computer won', {});
-				document.querySelector('#playagainBtn').style.display = 'block';
-				return;
-			}
-		});
-	});
-}; */
-
-const makeCpuGameboardClickable = (player, computer) => {
+const doCpuboardClickable = (player, computer) => {
 	document
 		.querySelector('#cpuboard-bx')
 		.addEventListener('click', function (ev) {
+			if (isGameOver) return;
+
 			const coor = ev.target.dataset.coor.split(',');
 
 			const thisCellIsYetHit = player.attack(coor[0], coor[1]);
 			if (thisCellIsYetHit) return;
 
 			if (computer.gameboard.allShipsHaveBeenSunk()) {
+				isGameOver = true;
 				PubSub.publish('Player won', {});
+				document.querySelector('#playagainBtn').style.display =
+					'inline';
 				return;
 			}
 
 			computer.attack();
 
 			if (player.gameboard.allShipsHaveBeenSunk()) {
+				isGameOver = true;
 				PubSub.publish('Computer won', {});
-				document.querySelector('#playagainBtn').style.display = 'block';
+				document.querySelector('#playagainBtn').style.display =
+					'inline';
 				return;
 			}
 		});
 };
 
-const addEventListenerToResetButton = () => {
+const doResetButtonClickable = () => {
 	document.querySelector('#resetBtn').addEventListener('click', () => {
 		Dock().reset();
 		Placeboard().reset();
@@ -62,39 +49,51 @@ const addEventListenerToResetButton = () => {
 	});
 };
 
-const addEventListenerToStartButton = (gb) => {
+const doStartButtonClickable = () => {
 	document.querySelector('#startBtn').addEventListener('click', () => {
-		Placeboard().setOwnShipPlacementsOnOtherGb(gb);
+		Placeboard().setOwnShipPlacementsOnOtherGb(playerGb);
+		randomPlaceShips(computerGb);
+
 		Dock().reset();
 		Placeboard().reset();
-
 		document.querySelector('#startBtn').disabled = true;
 
 		document.querySelector('#placement-screen').style.display = 'none';
 		document.querySelector('#game-screen').style.display = 'block';
+
+		isGameOver = false;
 	});
 };
 
-const addEventListenerToRandomButton = () => {
+const doRandomButtonClickable = () => {
 	document.querySelector('#randomBtn').addEventListener('click', () => {
+		Dock().disable();
 		Placeboard().reset();
 		randomPlaceShips(Placeboard());
-		Dock().disable();
 
 		document.querySelector('#startBtn').disabled = false;
 	});
 };
 
+const doPlayagainButtonClickable = () => {
+	document.querySelector('#playagainBtn').addEventListener('click', () => {
+		playerGb.reset();
+		computerGb.reset();
+		document.querySelector('#playagainBtn').style.display = 'none';
+
+		document.querySelector('#placement-screen').style.display = 'block';
+		document.querySelector('#game-screen').style.display = 'none';
+	});
+};
+
 const Game = () => {
+	// placement screen
 	Placeboard().render('pboard-bx');
 	Dock().render('dock-bx');
 
-	addEventListenerToResetButton();
-
-	//-----------------------------
-
-	const playerGb = Gameboard();
-	const computerGb = Gameboard(true);
+	// game screen
+	playerGb = Gameboard();
+	computerGb = Gameboard(true);
 
 	playerGb.render('playerboard-bx');
 	computerGb.render('cpuboard-bx');
@@ -102,12 +101,13 @@ const Game = () => {
 	const player = Player(playerGb, computerGb);
 	const computer = Player(computerGb, playerGb, 1);
 
-	addEventListenerToStartButton(playerGb);
-	addEventListenerToRandomButton();
+	// events
+	doResetButtonClickable();
+	doStartButtonClickable();
+	doRandomButtonClickable();
 
-	randomPlaceShips(computerGb);
-
-	makeCpuGameboardClickable(player, computer);
+	doCpuboardClickable(player, computer);
+	doPlayagainButtonClickable();
 };
 
 export default Game;
